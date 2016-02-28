5f22644b5f82edf0133b1169608de1fc8181f79b
maartjeth <maartjeterhoeve@gmail.com>
2016-02-28 23:02:31 +0100

    first part, till finds H matrix

--- assignment-3/gaussianDer2.m
+++ assignment-3/gaussianDer2.m
@@ -2,24 +2,24 @@
 % Frederik Harder - 10986847 - frederikharder@gmail.com
 % Maartje ter Hoeve - 10190015 - maartje.terhoeve@student.uva.nl
 
-function [imOut, Gd] = gaussianDer(image_path, G, sigma, conv_option)
+function [imOut, Gd] = gaussianDer2(image_path, G, sigma, dir, conv_option)
     %set default conv2 shape to valid
-    if nargin 4 5
+    if nargin < 5
         conv_option = 'valid';
     end
 
     % create gaussian derivative filter Gd from gaussian filter G
-    x = 2;%((1:size(G,2)) - ceil(size(G,2)/2))';
-    %if dir == 1
-        Gd = (-x / sigma^2 .* G); 
-        display('gd')
-        size(Gd)
-        
-    %end
+    % direction depending on x (=1) or y (=2)
+    if dir == 1
+      x = ((1:size(G,2)) - ceil(size(G,2)/2));       
+      Gd = (-x / sigma^2 .* G);         
+    end
     
-    %if dir == 2
-    %    Gd = (-x / sigma^2 .* G)';
-    %end
+    if dir == 2               
+        y = ((1:size(G,1)) - ceil(size(G,1)/2))';
+        Gd = (-y / sigma^2 .* G);  
+    end
+    
     % load image
     im = imread(image_path);
     im = rgb2gray(im);
--- assignment-3/harris_corner.m
+++ assignment-3/harris_corner.m
@@ -16,15 +16,25 @@ function [H] = harris_corner(im_path, sigma, kernel_length)
     [Ix, Iy] = get_i(im_path, sigma, filter_x, filter_y);
     [A, B, C] = get_elem_q(Ix, Iy, filter_x, filter_y);
     H = construct_h(A, B, C);
+    [r, c] = get_corners(H, kernel_length); % now I choose the kernel length for the filter to go over H, but we can change this
 end
 
 function [Ix, Iy] = get_i(im_path, sigma, filter_x, filter_y)
     % smoothed derivatives of the image: convolve the first order gaussian
     % derivative, Gd, with the image along the x or y direction   
    
-    % function copied from last week:    
-    Ix = gaussianDer(im_path, filter_x, sigma);
-    Iy = gaussianDer(im_path, filter_y, sigma);
+    % function copied from last week, but slightly modified to get the two different directions  
+    % use same to get the same dimensions
+    Ix = gaussianDer2(im_path, filter_x, sigma, 1, 'same'); % horizontal edge filter
+    Iy = gaussianDer2(im_path, filter_y, sigma, 2, 'same'); % vertical edge filter
+    
+    % show the images (part of the assignment, don't delete)
+    % figure, imshow(Ix);
+    % figure, imshow(Iy);
+    
+    % notes
+    % Ix: 576 * 720
+    % Iy: 576 * 720
 end
 
 function [A, B, C] = get_elem_q(Ix, Iy, filter_x, filter_y)
@@ -34,12 +44,15 @@ function [A, B, C] = get_elem_q(Ix, Iy, filter_x, filter_y)
     % B: multiply Ix and Iy and convolve it with a Gaussian
     % C: squaring Iy and convolving it with a Gaussian
     
-    filter_xy = filter_y * filter_x; % need to use this, as both directions?
-    A = conv2(Ix.^2, filter_xy, 'valid');
-    B = conv2(Ix*Iy, filter_xy, 'valid'); % correct multiplication?
-    C = conv2(Iy.^2, filter_xy, 'valid');
+    A = conv2(Ix.^2, filter_x, 'same');
+    B = conv2(Ix, filter_x, 'same') .* conv2(Iy, filter_y, 'same');
+    C = conv2(Iy.^2, filter_y, 'same'); 
+    
+    %size(A)
+    %size(B)
+    %size(C)    
 end
 
 function [H] = construct_h(A, B, C)
-    H = (A*B-B.^2) - 0.04*(A+C).^2; % doesn't work yet --> dimension error
+    H = (A.*B-B.^2) - 0.04*(A+C).^2;
 end
