bad735d980af386f93e84a1ac6d0fc9668554747
maartjeth <maartjeterhoeve@gmail.com>
2016-03-25 17:47:48 +0100

    merged code

--- final-project/feature_script.m
+++ final-project/feature_script.m
@@ -1,7 +1,7 @@
 %run('D:/Computer Vision/vlfeat-0.9.20/toolbox/vl_setup')    
 
 
-source_dirs = {'.\Caltech4\debug\source'};
+source_dirs = {'.\Caltech4\debug\source\airplanes_train', '.\Caltech4\debug\source\cars_train', '.\Caltech4\debug\source\faces_train', '.\Caltech4\debug\source\motorbikes_train'};
 target_dir = '.\Caltech4\debug\target';
 file_format = 'jpg';
 vocab_percentage = 0.5;
