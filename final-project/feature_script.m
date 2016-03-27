6430d0f1825539a9478d8cef37eb8e1484a0d41d
maartjeth <maartjeterhoeve@gmail.com>
2016-03-28 00:54:38 +0200

    test script

--- final-project/feature_script.m
+++ final-project/feature_script.m
@@ -1,14 +1,51 @@
 %run('D:/Computer Vision/vlfeat-0.9.20/toolbox/vl_setup')    
 
+% change the file names to where it's on your pc
+source_dirs = {'.\Caltech4\ImageData\airplanes_small_train', '.\Caltech4\ImageData\cars_small_train', '.\Caltech4\ImageData\faces_small_train', '.\Caltech4\ImageData\motorbikes_small_train'};
 
-%source_dirs = {'.\Caltech4\ImageData\airplanes_small_train', '.\Caltech4\ImageData\cars_small_train', '.\Caltech4\ImageData\faces_small_train', '.\Caltech4\ImageData\motorbikes_small_train'};
-source_dirs = {'.\Caltech4\ImageData\airplanes_small_train - kopie', '.\Caltech4\ImageData\cars_small_train - kopie', '.\Caltech4\ImageData\faces_small_train - kopie', '.\Caltech4\ImageData\motorbikes_small_train - kopie'};
 target_dir = '.\Caltech4\debug\target';
 file_format = 'jpg';
 vocab_percentage = double(1/3);
-vocab_size = 5;
-sift_type = 'dense_opponent_3';
+vocab_size = 400;
 test_dirs = {'.\Caltech4\ImageData\airplanes_test', '.\Caltech4\ImageData\cars_test', '.\Caltech4\ImageData\faces_test', '.\Caltech4\ImageData\motorbikes_test'};
 
+sift_type = 'dense_rgb_3';
+prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
+im_classification_merge(sift_type, vocab_size)
+
+sift_type = 'dense_RGB_3';
+prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
+im_classification_merge(sift_type, vocab_size)
+
+sift_type = 'ip_opponent_3';
+prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
+im_classification_merge(sift_type, vocab_size)
+
+sift_type = 'dense_opponent_3';
+prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
+im_classification_merge(sift_type, vocab_size)
+
+vocab_size = 800;
+sift_type = 'ip_rgb_3';
+prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
+im_classification_merge(sift_type, vocab_size)
+
+sift_type = 'ip_RGB_3';
+prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
+im_classification_merge(sift_type, vocab_size)
+
+sift_type = 'ip_opponent_3';
+prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
+im_classification_merge(sift_type, vocab_size)
+
+sift_type = 'dense_rgb_3';
+prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
+im_classification_merge(sift_type, vocab_size)
+
+sift_type = 'dense_RGB_3';
+prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
+im_classification_merge(sift_type, vocab_size)
+
+sift_type = 'dense_opponent_3';
 prepare_features(source_dirs, target_dir, file_format, vocab_percentage, vocab_size, sift_type, test_dirs);
 im_classification_merge(sift_type, vocab_size)
--- final-project/prepare_features.m
+++ final-project/prepare_features.m
@@ -6,16 +6,21 @@ function prepare_features(source_dirs, target_dir, file_format, vocab_percentage
 
     
     [vfiles, ffiles] = divide_data(source_dirs, file_format, vocab_percentage);
-
+    display('getting vocab')
     vocabulary = build_vocab(vfiles, sift_type, vocab_size);
+    display('getting train data')
     train_data = build_histograms(ffiles, vocabulary, sift_type);
     
+    display('saving train and vocab data')
     filename = strcat(target_dir, '\train_data_', sift_type, '_voc_', num2str(vocab_size),'.mat');
     save(filename, 'vocabulary', 'train_data');
     
+    display('getting test data')
     if nargin > 6 % get histogram description of test data
         tfiles = get_test_files(test_dirs, file_format);
+        display('building histograms for test')
         test_data = build_histograms(tfiles, vocabulary, sift_type);
+        display('saving test data')
         filename = strcat(target_dir, '\test_data_', sift_type, '_voc_', num2str(vocab_size),'.mat');
         save(filename, 'test_data');
     end
@@ -27,7 +32,7 @@ function [vfiles, ffiles] = divide_data(source_dirs, file_format, vocab_percenta
     ffiles = cell(length(source_dirs), 1);      % cell vector of vectors for each class
     
     for idx = 1:length(source_dirs)
-        display(source_dirs{idx})
+        %display(source_dirs{idx})
         im_paths = get_imagepaths(source_dirs{idx}, file_format); % get file paths
         
         im_paths = im_paths( randperm(length(im_paths)),: ); % shuffle file paths
@@ -36,21 +41,34 @@ function [vfiles, ffiles] = divide_data(source_dirs, file_format, vocab_percenta
         vfiles = [vfiles; im_paths(1:lastv,:)]; 
         ffiles{idx} = im_paths(lastv+1:end,:);
     end
-    
+    size(vfiles)
+    size(ffiles)
 end
 
 function vocabulary = build_vocab(im_paths, sift_type, vocab_size)
     num_channels = str2double(sift_type(end));
     vocab_features = zeros(0, 128);%*num_channels);
-    display('buidling vocab');
+    display('building vocab');
     
     for i=1:size(im_paths, 1)
         im = imread(im_paths{i});
         features = sift(im, sift_type);
         vocab_features(end+1:end+size(features, 2), :) = features';        
+    end  
+    
+   
+    subset_size = 100000;
+    if size(vocab_features, 1) > subset_size
+        rand = randperm(size(vocab_features, 1));
+        subset_vocab_features = vocab_features(rand(1:subset_size), :);
+    else
+        subset_vocab_features = vocab_features;
     end
     
-    [~, vocabulary] = kmeans(vocab_features, vocab_size, 'MaxIter', 500);
+    [vocabulary, ~] = vl_kmeans(double(subset_vocab_features'), vocab_size);
+    display('built vocab')
+    size(vocabulary)
+    %[~, vocabulary] = kmeans(vocab_features, vocab_size, 'MaxIter', 700);
 end
 
 function features = sift(im, type)
@@ -152,10 +170,11 @@ end
 
 function train_data = build_histograms(label_sets, vocabulary, sift_type)
     train_data = zeros(0, size(vocabulary, 1) + 1); % last index is the label
-    
+    %length(label_sets)
     for label = 1:length(label_sets)
         im_paths = label_sets{label};
-        label_data = zeros(length(im_paths), size(vocabulary, 1));
+        %size(im_paths)
+        label_data = zeros(length(im_paths), size(vocabulary, 2));
         
         for idx = 1:length(im_paths)
             im = imread(im_paths{idx});
@@ -169,11 +188,15 @@ end
 function hist = make_hist(im, sift_type, vocab)    
 
     desc = sift(im, sift_type);
-    words = knnsearch(vocab, double(desc'));
-    hist = zeros(1, size(vocab, 1));
-    for i = 1:size(words,1)
-        hist(words(i)) = hist(words(i)) + 1;
-    end
+    %size(vocab)
+    %size(desc)
+    words = dsearchn(vocab', double(desc)');
+    %words = knnsearch(vocab, double(desc'));
+    hist = histcounts(words, size(vocab, 2));
+    %hist = zeros(1, size(vocab, 1));
+    %for i = 1:size(words,1)
+    %    hist(words(i)) = hist(words(i)) + 1;
+    %end
     hist = hist ./ double(size(words,1));
 end
 
